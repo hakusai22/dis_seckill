89926bea898126506f885f5c41e56fc4617340fd
yinpeng8 <yinpeng8@hikvision.com.cn>
2022-02-26 11:52:54 +0800

    [add] 雪花算法初始化

---
+++ dis-seckill-gateway/src/main/java/com/seckill/dis/gateway/utils/AcquiredLockWorker.java
@@ -0,0 +1,16 @@
+package com.seckill.dis.gateway.utils;
+
+/**
+ * 获取锁后需要处理的逻辑
+ *
+ * @author yinpeng
+ */
+@FunctionalInterface
+public interface AcquiredLockWorker<T> {
+    /**
+     * 获取锁后处理的逻辑
+     * @return
+     * @throws Exception
+     */
+    T invokeAfterLockAcquire() throws Exception;
+}
